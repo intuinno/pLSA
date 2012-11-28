830d2342ec6759627741aaca8eedce25a54e15b1
intuinno <intuinno@gmail.com>
2012-11-28 00:52:16 -0500

    added M step

--- CF1.m
+++ CF1.m
@@ -154,22 +154,78 @@ for i=1:numIteration
 	
 	%First Calculate M_yz
 	
-	for countUser=1:numUser
+	for countItem=1:numMovie
 		
-		for countItem=1:numMovie
-			
-			down = 0;
-			
-			for countLC=1:numLatentClass
-	
-	
+		for countLC=1:numLatentClass
+
+			up = 0; 
+			down =0;
+
+			for countUser = 1 : numUser
+
+				up = up + ratings(countUser,countItem)*Q(countUser,countItem,countLC);
+				down = down + Q(countUser,countItem,countLC);
+
 			end
-			
+
+			M_yz(countItem,countLC) = up/down;
+
+
 		end
 		
 	end
 	
+	%Second Calculate Std_yz
+	
+	for countItem=1:numMovie
+		
+		for countLC=1:numLatentClass
+
+			up = 0; 
+			down =0;
+
+			for countUser = 1 : numUser
+
+				up = up + (ratings(countUser,countItem)-M_yz(countItem,countLC))^2*Q(countUser,countItem,countLC);
+				down = down + Q(countUser,countItem,countLC);
+
+			end
+
+			M_yz(countItem,countLC) = up/down;
+
+			
+		end
+		
+	end
 	 
+	 %Lastly Calculate Pzu
+
+	for countUser=1:numUser
+
+		down=0;
+		
+		for countLC=1:numLatentClass
+
+			up = 0; 
+			
+
+			for countItem = 1 : numMovie
+
+				up = up + Q(countUser,countItem,countLC);
+				down = down + Q(countUser,countItem,countLC);
+
+			end
+
+			Pzu(countUser,countLC) = up;
+
+			
+		end
+		
+		Pzu(countUser,:) = Pzu(countUser,:) / down;
+
+	end
+
+	
 	disp(i);
 	
 	%calculateM;
